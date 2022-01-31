LineageOS/android_device_xiaomi_lmi

lmi: Add libinit for variants handling
Change-Id: Ia990f08d046192a42c0d951a2ce8d06b8794f6f0

BoardConfig.mk
@@ -25,6 +25,10 @@ SOONG_CONFIG_XIAOMI_KONA_FOD_SIZE = 202
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lmi
+TARGET_RECOVERY_DEVICE_MODULES := libinit_lmi
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/lmi_defconfig
 

Official-Ayrton990/device_xiaomi_lmi

Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

BoardConfig.mk
@@ -15,6 +15,10 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_lmi
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_lmi
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/lmi_defconfig
 